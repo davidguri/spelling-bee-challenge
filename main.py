d3b861623ca1f23201b20a7849314dbccc406318
David Guri <Davidguri@yahoo.com>
2022-03-12 20:58:27 +0100

    fixed word file, broke filter

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import json
+import string
+import random
 
-# import wordlist from github.com/dwyl/english-words
-# filter all 4 letter and above words
-# pick a random letter
 # filter all words with that letter in them
 # create an array with these words
 # from the array, pick 6 random letters
@@ -10,9 +9,22 @@ import json
 # if user input lower than 4, contains other letters or doesn't contain main letter, it's wrong, else OK.
 # make a cli
 
-with open("words_dictionary.json", "r") as read_file:
-    words = json.load(read_file)
-    print("json load successful")
+# txt_file = "words.txt"
+# word_py_dict = {}
+
+# with open(txt_file) as fh:
+#     for line in fh:
+#         command, description = line.strip().split(None, 1)
+#         word_py_dict[command] = description.strip()
+        
+# output_file = open("words_dictionary.json", "w")
+# json.dump(word_py_dict, output_file, indent = 4, sort_keys = False)
+# output_file.close()
+# the code above was made to convert the words.txt file to json
+
+with open("words.txt", "r") as read_file:
+    words = read_file.read()
+    print("txt load successful")
     
 word_dict = words
 
@@ -21,3 +33,6 @@ def by_length(words):
 
 allowed_words = by_length(word_dict)
 print(allowed_words)
+
+main_letter = random.choice(string.ascii_uppercase)
+print("Main letter: " + main_letter)
