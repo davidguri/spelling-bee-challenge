2ff125d4615c5762e4bbfc01f4d3a49f7dfa3f76
David Guri <81036791+davidguri@users.noreply.github.com>
2022-03-12 16:38:10 +0100

    added the logic for the program

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import json
 
-print("Hello world")
+# import wordlist from github.com/dwyl/english-words
+# filter all 4 letter and above words
+# pick a random letter
+# filter all words with that letter in them
+# create an array with these words
+# from the array, pick 6 random letters
+# take user input
+# if user input lower than 4, contains other letters or doesn't contain main letter, it's wrong, else OK.
+# make a cli
