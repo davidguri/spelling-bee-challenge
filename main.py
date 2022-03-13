e085ea8dd97d1c15a1da62756dfdb602f95047aa
David Guri <Davidguri@yahoo.com>
2022-03-13 14:09:37 +0100

    .

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import json
 import string
 import random
+import time
 
+# filter all 4 letter and above words
+# pick a random letter
 # filter all words with that letter in them
 # create an array with these words
 # from the array, pick 6 random letters
@@ -9,30 +12,22 @@ import random
 # if user input lower than 4, contains other letters or doesn't contain main letter, it's wrong, else OK.
 # make a cli
 
-#txt_file = "words.txt"
-#word_py_dict = {}
+main_letter = random.choice(string.ascii_lowercase)
+print("\nMain letter: " + main_letter)
+# pick main letter
 
-#with open(txt_file) as fh:
-#    for line in fh:
-#        command, description = line.strip().split(None, 1)
-#        word_py_dict[command] = description.strip()
-        
-#output_file = open("words_dictionary.json", "w")
-#json.dump(word_py_dict, output_file, indent = 4, sort_keys = False)
-#output_file.close()
-# the code above was made to convert the words.txt file to json
-
-with open("words.txt", "r") as read_file:
-    words = read_file.read()
-    print("txt load successful")
+with open("words_dictionary.json", "r") as read_file:
+    words = json.load(read_file)
+    # print(words)
+# import wordlist
     
 word_dict = words
+# set the word dictionary
 
 def by_length(words):
     return [word for word in words if len(word) >= 4]
+# filter words by length
 
-allowed_words = by_length(word_dict)
-print(allowed_words)
-
-main_letter = random.choice(string.ascii_uppercase)
-print("Main letter: " + main_letter)
+by_length_words = by_length(word_dict)
+# print(by_length_words)
+# these are the allowed words (by length)
